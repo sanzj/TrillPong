cfdf54e451831ade6f97408fbc25c07e3f8055e4
The Trillest <sanzj96@gmail.com>
2017-04-09 03:24:44 -0400

    Options menu and Second Player
    The second player is not completely done as I need to finish the bounds exception

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -35,16 +35,33 @@ namespace Pong
     }
 
     public class Game
-    {
+    { // A better way to handle this would be a class that does all the menu and game instantiating and the game instances ,ie most of this, would be their own class, one for Ai and one for human player then I would call the correct class dependent on the options chosen in the main class. This would fix my having to use a dynamic object
+        enum GameState
+        {
+            Play,
+            Menu,
+            GameOver
+        }
+
         public enum KeyInputs
         {
             None,
             Up,
             Down
         }
+
+        enum OponentSetting
+        {
+            VersusAI,
+            VersusHuman
+        }
+
         public Canvas board = new Canvas();
+        GameState currentState = GameState.Menu;
+
+        OponentSetting setting; //Could've used an options class/Struct to hold information but scope rn is small enough for a bool in this class
         HumanStick playerOne; //On Right side
-        AIStick playerTwo; //On Left Side
+        dynamic playerTwo; //On Left Side, using dynamic kinda hurts the strongly typed characteristic of C#, I can breka this easily if I call something Im not supposed to. This means everything I call with this needs to be checked for or ensured It has it in its class
         Ball gameBall;
 
         Label playerOneLabel;
@@ -55,10 +72,11 @@ namespace Pong
         public KeyInputs playerOneKeyInput;
         public KeyInputs playerTwoKeyInput;
 
-        System.Windows.Threading.DispatcherTimer frameTimer = new System.Windows.Threading.DispatcherTimer();
+        System.Windows.Threading.DispatcherTimer frameTimer;
 
         void InitializeTimer()
         {
+            frameTimer = new System.Windows.Threading.DispatcherTimer();
             frameTimer.Interval = new TimeSpan(0, 0, 0, 0, 025);//Creates framespersecond by calling update and draw after ticks. every 25 millisecond is 40fps
             frameTimer.Tick += FrameTimer_Tick;
             frameTimer.Start();
@@ -72,32 +90,65 @@ namespace Pong
 
         public void OnKeyPressed(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Up)
+            if(currentState == GameState.Play)
             {
-                playerOneKeyInput = Game.KeyInputs.Up;
-                playerOneInputChanged();
+                if (e.Key == Key.Up)
+                {
+                    playerOneKeyInput = Game.KeyInputs.Up;
+                    PlayerOneInputChanged();
+                }
+                else if (e.Key == Key.Down)
+                {
+                    playerOneKeyInput = Game.KeyInputs.Down;
+                    PlayerOneInputChanged();
+                }
+
+                if(setting == OponentSetting.VersusHuman)
+                {
+                    if (e.Key == Key.W)
+                    {
+                        playerTwoKeyInput = Game.KeyInputs.Up;
+                        PlayerTwoInputChanged();
+                    }
+                    else if (e.Key == Key.S)
+                    {
+                        playerTwoKeyInput = Game.KeyInputs.Down;
+                        PlayerTwoInputChanged();
+                    }
+                }
+
             }
-            else if (e.Key == Key.Down)
-            {
-                playerOneKeyInput = Game.KeyInputs.Down;
-                playerOneInputChanged();
-            }
-            //Do the other buttons for when the second player is a human player
         }
 
         public void OnKeyUp(object sender, KeyEventArgs e)
         { //Ensures that input is only gotten when the key is being pressed and doesnt keep inputting after it is let go
-            if (e.Key == Key.Up || e.Key == Key.Down)
+            if(currentState == GameState.Play)
             {
-                playerOneKeyInput = Game.KeyInputs.None;
-                playerOneInputChanged();
+                if (e.Key == Key.Up || e.Key == Key.Down)
+                {
+                    playerOneKeyInput = Game.KeyInputs.None;
+                    PlayerOneInputChanged();
+                }
+
+                if(setting == OponentSetting.VersusHuman)
+                {
+                    if (e.Key == Key.W || e.Key == Key.S)
+                    {
+                        playerTwoKeyInput = Game.KeyInputs.None;
+                        PlayerTwoInputChanged();
+                    }
+                }
             }
-            //Do other buttons for when second player is a human player
         }
 
-        void playerOneInputChanged()
+        void PlayerOneInputChanged()
         {
             playerOne.input = playerOneKeyInput;
+        }
+
+        void PlayerTwoInputChanged()
+        {
+            playerTwo.input = playerTwoKeyInput;
         }
 
         void InitializeScoreTable()
@@ -109,6 +160,66 @@ namespace Pong
             board.Children.Add(playerOneLabel);
             board.Children.Add(playerTwoLabel);
             UpdateScore();
+        }
+
+        void InitializeGameState()
+        {
+            playerOne = new HumanStick(500, 200);
+            gameBall = new Ball();
+
+            if(setting == OponentSetting.VersusAI)
+                playerTwo = new AIStick(25, 350);
+            else
+                playerTwo = new HumanStick(25, 350);
+
+            board.Children.Clear();
+            board.Children.Add(playerOne.myShape);
+            board.Children.Add(playerTwo.myShape);
+            board.Children.Add(gameBall.myShape);
+            InitializeScoreTable();
+        }
+
+        void InitializeMenuState()
+        {
+            //Grid menuGrid = new Grid();
+
+            Button startButton = new Button();
+            startButton.Click += StartButton_Click;
+            startButton.Content = "Start Game";
+            Canvas.SetTop(startButton, 350);
+            Canvas.SetLeft(startButton, 225);
+
+            Button PlayerButton = new Button();
+            PlayerButton.Click += PlayerButton_Click;
+            PlayerButton.Content = "Player";
+            Canvas.SetTop(PlayerButton, 200);
+            Canvas.SetLeft(PlayerButton, 200);
+
+            Button AIButton = new Button();
+            AIButton.Click += AIButton_Click;
+            AIButton.Content = "AI";
+            Canvas.SetTop(AIButton, 200);
+            Canvas.SetLeft(AIButton, 300);
+
+            board.Children.Add(startButton);
+            board.Children.Add(PlayerButton);
+            board.Children.Add(AIButton);     
+        }
+
+        private void AIButton_Click(object sender, RoutedEventArgs e)
+        {
+            setting = OponentSetting.VersusAI;
+        }
+
+        private void PlayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            setting = OponentSetting.VersusHuman;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentState = GameState.Play;
+            ChangeState();
         }
 
         void ServeBall()
@@ -126,16 +237,6 @@ namespace Pong
 
         public Game()
         {
-            playerOne = new HumanStick(500,200);
-            playerTwo = new AIStick(25, 350);
-            gameBall = new Ball();
-
-            board.Children.Add(playerOne.myShape);
-            board.Children.Add(playerTwo.myShape);
-            board.Children.Add(gameBall.myShape);
-
-            InitializeScoreTable();
-
             InitializeTimer();
         }
 
@@ -194,7 +295,7 @@ namespace Pong
             return false;
         }
 
-        bool StickHasHitTop()
+        bool StickOneHasHitTop()
         {
             if (playerOne.coordinates.Y < 0)
             {
@@ -203,7 +304,7 @@ namespace Pong
             return false;
         }
 
-        bool StickHasHitBottom()
+        bool StickOneHasHitBottom()
         {
             double maxHeight = board.ActualHeight;
             if (playerOne.coordinates.Y + playerOne.height > maxHeight)
@@ -213,16 +314,40 @@ namespace Pong
             return false;
         }
 
+        bool StickTwoHasHitTop()
+        {
+            if (playerTwo.coordinates.Y < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool StickTwoHasHitBottom()
+        {
+            double maxHeight = board.ActualHeight;
+            if (playerTwo.coordinates.Y + playerTwo.height > maxHeight)
+            {
+                return true;
+            }
+            return false;
+        }
+
         void HandleCollisions()
         {
-            if (PlayerHasHitBall())
+            if (PlayerOneHasHitBall())
             {
                 gameBall.BounceOffHit(true);
-                if (PlayerOneHasHitBall())
-                {
-                    gameBall.coordinates.X = playerOne.coordinates.X - gameBall.width;
+                gameBall.coordinates.X = playerOne.coordinates.X - gameBall.width;
+
+                if (setting == OponentSetting.VersusAI)
                     playerTwo.targetY = FindYIntersection();
-                }
+            }
+
+            if (PlayerTwoHasHitBall())
+            {
+                gameBall.BounceOffHit(true);
+                gameBall.coordinates.X = playerTwo.coordinates.X + gameBall.width;
             }
 
             if (BallHasHitTopOrBottom())
@@ -230,13 +355,22 @@ namespace Pong
                 gameBall.BounceOffHit(false);
             }
 
-            if (StickHasHitTop())
+            if (StickOneHasHitTop())
             {
                 playerOne.coordinates.Y = 0;
             }
-            else if (StickHasHitBottom())
+            else if (StickOneHasHitBottom())
             {
                 playerOne.coordinates.Y = board.ActualHeight - playerOne.height;
+            }
+            //The Second stick checks are not working it may have something to do with stick two being dynamic IDK tho
+            if (StickTwoHasHitTop())
+            {
+                playerTwo.coordinates.Y = 0;
+            }
+            else if (StickTwoHasHitBottom())
+            {
+                playerTwo.coordinates.Y = board.ActualHeight - playerTwo.height;
             }
 
             if (PlayerOneHasScored())
@@ -254,8 +388,7 @@ namespace Pong
         }
 
         double FindYIntersection()
-        {
-            //Currently does not take into account the speed change on a hit in ball. I could maybe use the same random seed as in ball or not IDk. Either its off sometimes rn
+        {//Currently does not take into account the speed change on a hit in ball. I could maybe use the same random seed as in ball or not IDk. Either its off sometimes rn
             Double targetX = playerTwo.coordinates.X;
             double x = gameBall.coordinates.X;
             double y = gameBall.coordinates.Y;
@@ -276,17 +409,46 @@ namespace Pong
 
         void Update()
         {
-            playerOne.Update();
-            playerTwo.Update();
-            gameBall.Update();
-            HandleCollisions();
+            if(currentState == GameState.Play)
+            {
+                playerOne.Update();
+                playerTwo.Update();
+                gameBall.Update();
+                HandleCollisions();
+            }
+        }
+
+        void ChangeState()
+        {
+            if (currentState == GameState.Play)
+            {
+                frameTimer.Start(); //This seems to not be encessary. Odd
+                InitializeGameState();
+            }               
+            else if (currentState == GameState.Menu)
+            {
+                frameTimer.Stop();
+                InitializeMenuState();
+            }                
+            else if (currentState == GameState.GameOver)
+                throw new NotImplementedException();
+        }
+
+        void DrawMenu()
+        {
+                InitializeMenuState();
         }
 
         void Draw()
         {
-            playerOne.Draw();
-            playerTwo.Draw();
-            gameBall.Draw();
+            if (currentState == GameState.Play)
+            {
+                playerOne.Draw();
+                playerTwo.Draw();
+                gameBall.Draw();
+            }
+            else if (currentState == GameState.Menu)
+                DrawMenu();
         }
     }
 
@@ -294,11 +456,14 @@ namespace Pong
     {
         public Point coordinates;// May want to use my onw xand y coordinates. Also may want to turn my fields into properties
         public Shape myShape;
+        public readonly int height;
+        public readonly int width;
 
-        //protected GameShape(int x, int y)
-        //{
-        //    //Make a constructor that can be used in children and specialized in base children classes
-        //}
+        protected GameShape(int shapeHeight, int shapeWidth)
+        {
+            height = shapeHeight;
+            width = shapeWidth;
+        }
 
         protected abstract void MoveCoordinates();
 
@@ -317,23 +482,28 @@ namespace Pong
     public abstract class Stick : GameShape
     {//Everything from GameShape is automatically passed in although it is not mentioned. All subclasses must still implement them
         protected const int movementSpeed = 5;
-        public readonly int height = 75;
-        public readonly int width = 10;
+
+        protected Stick(int x, int y) : base(75, 10)
+        {
+            myShape = new Rectangle();
+            myShape.Height = height;
+            myShape.Width = width;
+            myShape.Fill = Brushes.Black;
+            Initialize(x, y);
+        }
+
+        void Initialize(int xCoord, int yCoord)
+        {
+            coordinates.X = xCoord;
+            coordinates.Y = yCoord;
+        }
     }
 
     public class HumanStick : Stick
     {
         public Game.KeyInputs input;
 
-        public HumanStick(int x, int y)
-        {
-            myShape = new Rectangle();
-            coordinates.X = x;
-            coordinates.Y = y;
-            myShape.Height = height;
-            myShape.Width = width;
-            myShape.Fill = Brushes.Black;
-        }
+        public HumanStick(int x, int y) : base(x, y) { }
 
         protected override void MoveCoordinates()
         {
@@ -352,15 +522,7 @@ namespace Pong
     {
         public double? targetY;
 
-        public AIStick(int x, int y)
-        {
-            myShape = new Rectangle();
-            coordinates.X = x;
-            coordinates.Y = y;
-            myShape.Height = height;
-            myShape.Width = width;
-            myShape.Fill = Brushes.Black;
-        }
+        public AIStick(int x, int y) : base(x, y) { }
 
         protected override void MoveCoordinates()
         {
@@ -380,12 +542,10 @@ namespace Pong
 
     public class Ball : GameShape
     {
-        public readonly int height = 25;
-        public readonly int width = 25;
         public int xSpeed = 5;
         public int ySpeed = 2;
 
-        public Ball()
+        public Ball() : base (25, 25)
         {
             myShape = new Ellipse();
             myShape.Height = height;
